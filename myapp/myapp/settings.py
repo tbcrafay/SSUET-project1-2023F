0ba660f560852241ba89b33e19d4db9d3af96c4b
tbcrafay <abdulrafaymehmood123@gmail.com>
2023-12-23 17:04:49 +0500

    only for the show, will correct them after learning more

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR,"static",
+]
+
+
+
+
