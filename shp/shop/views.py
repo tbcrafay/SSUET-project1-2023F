ae0f15710b2acdcb2ba4449e5bf395971e245e51
tbcrafay <abdulrafaymehmood123@gmail.com>
2023-12-26 13:12:18 +0500

    just added a desing will later customize it according to my topic

---
+++ shp/shop/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'shop/Index.html')
+
+def about(request):
+    return HttpResponse("We are at about")
+
+def contact(request):
+    return HttpResponse("We are at contact")
+
+def tracker(request):
+    return HttpResponse("We are at tracker")
+
+def search(request):
+    return HttpResponse("We are at search")
+
+def productView(request):
+    return HttpResponse("We are at product view")
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
+
+
+
